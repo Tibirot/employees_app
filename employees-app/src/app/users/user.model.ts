5d5d0ba5894e465098c13e115d7d885308be4731
Socea Tiberiu <soceatiberiuv@gmail.com>
2020-01-07 18:19:13 +0200

    Update user.model.ts

--- employees-app/src/app/users/user.model.ts
+++ employees-app/src/app/users/user.model.ts
@@ -1,13 +1,6 @@
 export class User {
   private _age: number = this.age;
-  //TESTING    
-
-> 
-
-> 
-
-> PR
-
+  //TESTING
   constructor(private _userName: string,private _password: string, private _description: string, private _birthDate: Date) {
   }
   get userName(): string {
